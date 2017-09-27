9b56028e5263c8265b0d7d07a41d4d358b44c201
Rohan Mundhey <mundheyrohan@gmail.com>
2017-09-27 07:44:14 +0530

    Day Thirteen

---
+++ day 13.py
@@ -0,0 +1,38 @@
+from abc import ABCMeta, abstractmethod
+class Book:
+    __metaclass__ = ABCMeta
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+
+    def __init__(self,tit,aut,pri):
+        self.cost=pri
+
+        Book.__init__(self,tit,aut)
+    def display(self):
+        print "Title:",self.title
+        print "Author:",self.author
+        print "Price:",self.cost
+
+
+
+
+
+
+
+
+
+
+
+
+
+title=raw_input()
+author=raw_input()
+price=int(raw_input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
