64312a1cdf30d5fe38f82b1ea2fc384471de52f8
Rohan Mundhey <mundheyrohan@gmail.com>
2017-10-31 09:00:04 +0530

    day 29

---
+++ day 29.py
@@ -0,0 +1,28 @@
+
+
+import sys
+
+
+t = int(raw_input())
+for a0 in range(t):
+    n,k = raw_input().strip().split(' ')
+    n,k = [int(n),int(k)]
+
+
+    maxi=0
+    for p in range(1,n):
+
+        for q in range(p+1,n+1):
+
+
+         new_max=p & q
+         if k > new_max > maxi:
+             maxi = new_max
+
+    print maxi
+
+
+
+
+ 
+
